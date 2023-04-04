4fd92e455f65676373b51ef3167c45ececa90fe7
sindhupriya9628 <105417653+sindhupriya9628@users.noreply.github.com>
2023-04-04 15:33:43 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+select * from exammaster order by exammasterid desc 
+
+identifier1 - eventid
+
+identifier2 - eventstrcutureid
+
+identifier3 - CourseId
+
+select * from StudentMaster
+
+select * from tago
